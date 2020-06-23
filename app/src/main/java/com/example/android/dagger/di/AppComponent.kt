a91739789bfa2fd7691c5d7bf0d33a4e4b449d57
Devansh-Maurya <devansh233@gmail.com>
2020-06-23 13:00:32 +0530

    Added DI to LoginActivity

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,7 +1,9 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationComponent
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -20,6 +22,7 @@ interface AppComponent {
     }
 
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 
     fun inject(activity: MainActivity)
 }
--- app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
+++ app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
@@ -1,10 +1,12 @@
 package com.example.android.dagger.di
 
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.registration.RegistrationComponent
 import dagger.Module
 
 /**
  * Created by Devansh on 23/6/20
  */
 
-@Module(subcomponents = [RegistrationComponent::class])
+@Module(subcomponents = [RegistrationComponent::class, LoginComponent::class])
 class AppSubcomponents
