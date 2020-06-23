3415bc8f6bff84dfcde2019a07322270d2ed091c
Devansh-Maurya <devansh233@gmail.com>
2020-06-23 12:50:41 +0530

    Completed DI in registration flow

---
+++ app/src/main/java/com/example/android/dagger/di/ActivityScope.kt
@@ -0,0 +1,12 @@
+package com.example.android.dagger.di
+
+import javax.inject.Scope
+
+/**
+ * Created by Devansh on 23/6/20
+ */
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class ActivityScope
---
+++ app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
@@ -0,0 +1,10 @@
+package com.example.android.dagger.di
+
+import dagger.Module
+
+/**
+ * Created by Devansh on 23/6/20
+ */
+
+@Module(subcomponents = [RegistrationComponent::class])
+class AppSubcomponents
